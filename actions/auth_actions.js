9dc1696717d1e96bb843e6f364b6d7684557cb36
Ronny Ewanek <ronny.ewanek@gmail.com>
2017-07-24 11:59:13 -0500

    facebook login screen works

--- actions/auth_actions.js
+++ actions/auth_actions.js
@@ -1,14 +1,29 @@
 import { AsyncStorage } from 'react-native';
+import { Facebook } from 'expo';
 
 import  {
-	FACEBOOK_LOGIN_SUCCESS
+	FACEBOOK_LOGIN_SUCCESS,
+	FACEBOOK_LOGIN_FAIL
 } from './types';
 
 export const facebookLogin = () => async dispatch => {
 	let token = await AsyncStorage.getItem('fb_token'); 
 	if (token) {
-		// Dispatch action 
+		dispatch({ type: FACEBOOK_LOGIN_SUCCESS, payload: token });
 	} else {
-		// Start FB Login
+		doFacebookLogin(dispatch);
 	}
+};
+
+const doFacebookLogin = async dispatch => {
+	let { type, token } = await Facebook.logInWithReadPermissionsAsync('1513058672101640', {
+		permissions: ['public_profile']
+	}); 
+
+	if(type === 'cancel') {
+		return dispatch({ type: FACEBOOK_LOGIN_FAIL });
+	} 
+
+	await AsyncStorage.setItem('fb_token', token);
+	dispatch({ type: FACEBOOK_LOGIN_SUCCESS, payload: token }); 
 };
