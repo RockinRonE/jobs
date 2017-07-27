d52d8f219a9fc8bd85eccb4bd072b4551acfb879
Ronny Ewanek <ronny.ewanek@gmail.com>
2017-07-27 10:56:14 -0500

    add redux persist

--- reducers/likes_reducer.js
+++ reducers/likes_reducer.js
@@ -1,4 +1,5 @@
 import _ from 'lodash';
+import { REHYDRATE } from 'redux-persist/constants';
 
 import {
 	LIKE_JOB,
@@ -7,6 +8,8 @@ import {
 
 export default function(state = [], action) {
 	switch(action.type) {
+		case REHYDRATE:
+			return action.payload.likedJobs || [];
 		case LIKE_JOB: 
 			return _.uniqBy([
 				action.payload, ...state
--- store/index.js
+++ store/index.js
@@ -1,6 +1,8 @@
 import { createStore, compose, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import reducers from '../reducers';
+import { persistStore, autoRehydrate } from 'redux-persist';
+import { AsyncStorage } from 'react-native'; 
 
 const store = createStore(
 	// default reducers
@@ -8,9 +10,12 @@ const store = createStore(
 	// default state
 	{},
 	compose(
-		applyMiddleware(thunk)
+		applyMiddleware(thunk),
+		autoRehydrate()
 	)
 
 );
+
+persistStore(store, { storage: AsyncStorage, whitelist: ['likedJobs'] });
 
 export default store; 
